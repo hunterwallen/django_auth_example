96ae06f8eacaaf3f1886485d65c527d1e23af4e5
Hunter Wallen <hunterwallen67@gmail.com>
2021-05-27 09:29:44 -0600

    commented in code

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('api/useraccount', views.UserAccountList.as_view(), name='useraccount_list'), # api/contacts will be routed to the ContactList view for handling
-    path('api/useraccount/<int:pk>', views.UserAccountDetail.as_view(), name='useraccount_detail'), # api/contacts will be routed to the ContactDetail view for handling
-    path('api/useraccount/login', csrf_exempt(views.check_login), name="check_login")
+    path('api/useraccount', views.UserAccountList.as_view(), name='useraccount_list'),
+    path('api/useraccount/<int:pk>', views.UserAccountDetail.as_view(), name='useraccount_detail'),
+    path('api/useraccount/login', csrf_exempt(views.check_login), name="check_login") # api/useraccount/login will be routed to the check_login function for auth
 ]
--- auth_api/views.py
+++ auth_api/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserAccountSerializer
 from .models import UserAccount
+
+### ALLOWS YOU TO CREATE AND CHECK PASSWORDS
 from django.contrib.auth.hashers import make_password, check_password
+### ALLOWS YOU TO SEND JSON AS A RESPONSE
 from django.http import JsonResponse
+### ALLOWS YOU TO TRANSLATE DICTIONARIES INTO JSON DATA
 import json
 
 
 
 class UserAccountList(generics.ListCreateAPIView):
-    queryset = UserAccount.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
-    serializer_class = UserAccountSerializer # tell django what serializer to use
-
+    queryset = UserAccount.objects.all().order_by('id')
+    serializer_class = UserAccountSerializer
 
 
 class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -19,20 +22,24 @@ class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserAccountSerializer
 
 
+
+### THIS IS THE FUNCTION THAT PERFORMS AUTH
 def check_login(request):
+        #IF A GET REQUEST IS MADE, RETURN AN EMPTY {}
     if request.method=='GET':
         return JsonResponse({})
-        #grab user object with matching email
+
+        #CHECK IF A PUT REQUEST IS BEING MADE
     if request.method=='PUT':
-        jsonRequest = json.loads(request.body)
-        email = jsonRequest['email']
-        password = jsonRequest['password']
-        if UserAccount.objects.get(email=email):
-                        #compare passwords
-            user = UserAccount.objects.get(email=email)
-            if check_password(password, user.password):
-                return JsonResponse({'id': user.id, 'email': user.email})
-            else:
+
+        jsonRequest = json.loads(request.body) #make the request JSON format
+        email = jsonRequest['email'] #get the email from the request
+        password = jsonRequest['password'] #get the password from the request
+        if UserAccount.objects.get(email=email): #see if email exists in db
+            user = UserAccount.objects.get(email=email)  #find user object with matching email
+            if check_password(password, user.password): #check if passwords match
+                return JsonResponse({'id': user.id, 'email': user.email}) #if passwords match, return a user dict
+            else: #passwords don't match so return empty dict
                 return JsonResponse({})
-        else:
+        else: #if email doesn't exist in db, return empty dict
             return JsonResponse({})
